40fb82fcdc35f369519d17f7e60224613d6682bc
nicwilson <nic.d.wilson@gmail.com>
2019-08-29 12:08:47 +1200

    Fix 404 page

--- 404.php
+++ 404.php
@@ -8,9 +8,11 @@
  */
 
 get_header();
+
+get_template_part( 'partial-banner-header');
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area alignwide">
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
@@ -21,35 +23,13 @@ get_header();
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'futurelab-base' ); ?></p>
 
+                    <p>
 					<?php
 					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'futurelab-base' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$futurelab_base_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'futurelab-base' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$futurelab_base_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+                    </p>
+                    
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
