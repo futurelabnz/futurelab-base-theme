94f427391d4395c4e02ca15895b7e19b9664feba
zerel <hankzang@gmail.com>
2020-01-22 13:43:29 +1300

    banner header style restructure remove unused code move all style utility to _utils.scss file

--- template-parts/partial-banner-header.php
+++ template-parts/partial-banner-header.php
@@ -9,7 +9,7 @@ $thumbnail_url = '';
     <div class="banner-header-image"
          style="background-image: url('<?php echo $thumbnail_url; ?>');">
         <div class="banner-header-overlay">
-            <header class="grid-container grid-margin-x alignwide">
+            <div class="grid-x banner-header-titles-container alignwide">
                 <div class="banner-header-titles">
 					<?php
 					$title_meta = get_futurelab_title_meta( $post );
@@ -19,15 +19,15 @@ $thumbnail_url = '';
 						echo '<div class="title-meta">' . $title_meta . '</div>';
 					endif;
 					if ( ! empty( $title ) ):
-						echo '<h1>' . $title . '</h1>';
+						echo '<h1 class="title font-weight-500">' . $title . '</h1>';
 					endif;
 					if ( ! empty( $sub_title ) ):
-						echo '<h2>' . $sub_title . '</h2>';
+						echo '<h3 class="subtitle font-weight-500">' . $sub_title . '</h3>';
 					endif;
 					?>
 					<?php echo get_futurelab_menu( $post->ID, 'fl_banner_menu', 'banner-header-menu' ); ?>
                 </div>
-            </header>
+            </div>
         </div>
         <div class="down-arrow-container hide-for-small-only"
              style="background-image: url( '<?php echo get_stylesheet_directory_uri() . '/assets/images/icon_arrow_scroll_down.svg'; ?>' );">
