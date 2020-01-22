358422c56e39ecf00d80a7b05d88282c68bb6974
zerel <hankzang@gmail.com>
2020-01-22 17:49:07 +1300

    remove wordpress wp-embed.min.js remove recent_comments_style in page

--- functions.php
+++ functions.php
@@ -359,6 +359,18 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
+// to remove wp-embed.min.js 
+function fl_deregister_scripts(){
+	wp_deregister_script( 'wp-embed' );
+  }
+  add_action( 'wp_footer', 'fl_deregister_scripts' );
+
+function fl_remove_recent_comments_style() {
+    global $wp_widget_factory;
+    remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+}
+add_action('widgets_init', 'fl_remove_recent_comments_style');
+
 // /**
 //  * remove woocommerce theme color
 //  */
