1a5963af7f7382b3dcb93bc6e3feac29cc6ce332
zerel <hankzang@gmail.com>
2020-02-24 15:31:02 +1300

    delete if condition for blocks

--- index.php
+++ index.php
@@ -42,8 +42,6 @@ get_header();
 
 						$list_items_markup = '';
 
-						$excerpt_length = '50';
-
 						foreach ( $recent_posts as $post ) {
 
 							$thumbnail = get_the_post_thumbnail( $post, 'large' ); // change to "large" because i need image to cover the full column, Hank
@@ -86,67 +84,10 @@ get_header();
 								$title
 							);
 
-							if (
-								isset( $attributes['displayPostContent'] ) && $attributes['displayPostContent']
-								&& isset( $attributes['displayPostContentRadio'] ) && 'excerpt' == $attributes['displayPostContentRadio']
-							) {
-								$post_excerpt = $post->post_excerpt;
-								if ( ! ( $post_excerpt ) ) {
-									$post_excerpt = $post->post_content;
-								}
-								$trimmed_excerpt = esc_html( wp_trim_words( $post_excerpt, $excerpt_length, ' &hellip; ' ) );
-
-								$list_items_markup .= sprintf(
-									'<div class="wp-block-latest-posts__post-excerpt">%1$s',
-									$trimmed_excerpt
-								);
-
-								if ( strpos( $trimmed_excerpt, ' &hellip; ' ) !== false ) {
-									$list_items_markup .= sprintf(
-										'<a href="%1$s">%2$s</a></div>',
-										esc_url( get_permalink( $post ) ),
-										__( 'Read More' )
-									);
-								} else {
-									$list_items_markup .= sprintf(
-										'</div>'
-									);
-								}
-							}
-
-							if (
-								isset( $attributes['displayPostContent'] ) && $attributes['displayPostContent']
-								&& isset( $attributes['displayPostContentRadio'] ) && 'full_post' == $attributes['displayPostContentRadio']
-							) {
-								$list_items_markup .= sprintf(
-									'<div class="wp-block-latest-posts__post-full-content">%1$s</div>',
-									wp_kses_post( html_entity_decode( $post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) )
-								);
-							}
-
 							$list_items_markup .= "</div></li>\n";
 						}
 
 						$class = 'wp-block-latest-posts wp-block-latest-posts__list is-grid columns-3 has-dates';
-						if ( isset( $attributes['align'] ) ) {
-							$class .= ' align' . $attributes['align'];
-						}
-
-						if ( isset( $attributes['postLayout'] ) && 'grid' === $attributes['postLayout'] ) {
-							$class .= ' is-grid';
-						}
-
-						if ( isset( $attributes['columns'] ) && 'grid' === $attributes['postLayout'] ) {
-							$class .= ' columns-' . $attributes['columns'];
-						}
-
-						if ( isset( $attributes['displayPostDate'] ) && $attributes['displayPostDate'] ) {
-							$class .= ' has-dates';
-						}
-
-						if ( isset( $attributes['className'] ) ) {
-							$class .= ' ' . $attributes['className'];
-						}
 
 						$block_content = sprintf(
 							'<ul class="%1$s">%2$s</ul>',
