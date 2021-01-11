0251d40e40031642689435242a756027a7031925
developerjared <developerjared@gmail.com>
2021-01-11 17:51:12 +1300

    Just some starting point

---
+++ inc/futurelab/Futurelab.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class FutureLab
+{
+    // class property declaration
+    public $var = 'a default value';
+
+    // constructor?
+
+    // public method declarations
+
+    // GETTERS
+    public function displayVar() {
+        echo $this->var;
+    }
+
+    // SETTERS
+
+    // Private Method Declarations?
+}
---
+++ inc/futurelab/config.php
@@ -0,0 +1,4 @@
+<?php
+
+$futurelab_elements = [];
+
