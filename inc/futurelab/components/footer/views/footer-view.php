791e4fa51f16e5db652583969b6137f22c150451
zerel <hankzang@gmail.com>
2021-01-19 17:53:29 +1300

    small spacing change

--- inc/futurelab/components/footer/views/footer-view.php
+++ inc/futurelab/components/footer/views/footer-view.php
@@ -1,7 +1,7 @@
 <footer class="pt-4 my-md-5 pt-md-5 border-top">
   <div class="container">
       <div class="row">
-        <div class="col-12 col-md">
+        <div class="col-12 col-lg-6 col-md">
           <?php 
               $widget_area = "footer_widgets_area_1";
               if ( is_active_sidebar( $widget_area ) ) {
